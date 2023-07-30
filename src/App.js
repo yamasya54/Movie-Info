57cb46ff520b245d62777ba58d4304d7ce27e448
yamasya54 <yavuzamasya1@gmail.com>
2023-07-30 15:47:58 +0300

    sa

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Main from "./pages/Main";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+function App() {
+  return (
+    <div className="App my_card m-auto mt-5 ">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
